bb388505e1dd313d96945c5fab92828699bfc934
wangkai <123940232@qq.com>
2015-12-06 22:32:31 +0800

    recyclerDemo

--- recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.recyclerviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+//        findViewById(R.id.btn_load).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                loadImages();
+//            }
+//        });
+        Fresco.initialize(this);
+        loadImages();
     }
+
+    private void loadImages() {
+        List<String> images = new ArrayList<>();
+        images.add("http://img5.imgtn.bdimg.com/it/u=1478257864,2882073929&fm=21&gp=0.jpg");
+        images.add("http://img0.imgtn.bdimg.com/it/u=1231062057,3852413437&fm=21&gp=0.jpg");
+        images.add("http://img5.imgtn.bdimg.com/it/u=1020667791,3260921600&fm=21&gp=0.jpg");
+        images.add("http://img4.imgtn.bdimg.com/it/u=828291890,997706858&fm=21&gp=0.jpg");
+images.add("http://img5.imgtn.bdimg.com/it/u=1478257864,2882073929&fm=21&gp=0.jpg");
+        images.add("http://img0.imgtn.bdimg.com/it/u=1231062057,3852413437&fm=21&gp=0.jpg");
+        images.add("http://img5.imgtn.bdimg.com/it/u=1020667791,3260921600&fm=21&gp=0.jpg");
+        images.add("http://img4.imgtn.bdimg.com/it/u=828291890,997706858&fm=21&gp=0.jpg");
+images.add("http://img5.imgtn.bdimg.com/it/u=1478257864,2882073929&fm=21&gp=0.jpg");
+        images.add("http://img0.imgtn.bdimg.com/it/u=1231062057,3852413437&fm=21&gp=0.jpg");
+        images.add("http://img5.imgtn.bdimg.com/it/u=1020667791,3260921600&fm=21&gp=0.jpg");
+        images.add("http://img4.imgtn.bdimg.com/it/u=828291890,997706858&fm=21&gp=0.jpg");
+images.add("http://img5.imgtn.bdimg.com/it/u=1478257864,2882073929&fm=21&gp=0.jpg");
+        images.add("http://img0.imgtn.bdimg.com/it/u=1231062057,3852413437&fm=21&gp=0.jpg");
+        images.add("http://img5.imgtn.bdimg.com/it/u=1020667791,3260921600&fm=21&gp=0.jpg");
+        images.add("http://img4.imgtn.bdimg.com/it/u=828291890,997706858&fm=21&gp=0.jpg");
+images.add("http://img5.imgtn.bdimg.com/it/u=1478257864,2882073929&fm=21&gp=0.jpg");
+        images.add("http://img0.imgtn.bdimg.com/it/u=1231062057,3852413437&fm=21&gp=0.jpg");
+        images.add("http://img5.imgtn.bdimg.com/it/u=1020667791,3260921600&fm=21&gp=0.jpg");
+        images.add("http://img4.imgtn.bdimg.com/it/u=828291890,997706858&fm=21&gp=0.jpg");
+images.add("http://img5.imgtn.bdimg.com/it/u=1478257864,2882073929&fm=21&gp=0.jpg");
+        images.add("http://img0.imgtn.bdimg.com/it/u=1231062057,3852413437&fm=21&gp=0.jpg");
+        images.add("http://img5.imgtn.bdimg.com/it/u=1020667791,3260921600&fm=21&gp=0.jpg");
+        images.add("http://img4.imgtn.bdimg.com/it/u=828291890,997706858&fm=21&gp=0.jpg");
+
+        RvAdapter adapter = new RvAdapter(images);
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
 }
--- recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/RvAdapter.java
+++ recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/RvAdapter.java
@@ -1,8 +1,67 @@
 package com.example.recyclerviewdemo;
 
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 /**
  * User: WangKai(123940232@qq.com)
  * 2015-12-06 10:07
  */
-public class RvAdapter {
-}  
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
+    private List<String> mImageUrls;
+
+    public RvAdapter(List<String> mImageUrls) {
+        this.mImageUrls = mImageUrls;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = View.inflate(parent.getContext(), R.layout.image_item, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final Uri uri = Uri.parse(mImageUrls.get(position));
+
+        holder.mImageView.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = holder.itemView.getWidth();
+                int perWidth = width / 3;
+                Picasso.with(holder.mImageView.getContext()).load(uri).centerCrop().resize(perWidth, perWidth).into(holder.mImageView);
+                holder.mSDV.setImageURI(uri);
+
+            }
+        });
+//        holder.mImageView.setImageResource(R.mipmap.ic_launcher);
+        Log.d("way", position + "");
+//        holder.mSDV.setImageURI(uri);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mImageUrls.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView mImageView;
+        SimpleDraweeView mSDV;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mImageView = (ImageView) itemView.findViewById(R.id.iv);
+            mSDV = (SimpleDraweeView) itemView.findViewById(R.id.sdv);
+        }
+    }
+
+}
